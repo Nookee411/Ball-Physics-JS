48dded25369d09f8625e5fa744966a71d33bd0cd
Nookee411 <daniil43rus@list.ru>
2020-10-01 12:38:40 +0300

    Code refactored

--- script.js
+++ script.js
@@ -1,152 +1,182 @@
 (()=> {
-
-    //TODO Add left button check for impulse
     //TODO Right button for ball dragging (force will be equal to potential energy
-    const config = {
-        TWO_PI : Math.PI*2,
-        color: "rgba(255,236,140,0.85)", //"rgba(0,0,0,0.8)",
-        radius :100,
-        airX : 0.99,
-        air : 0.78,
-        acceleration : 0.5,
-        maxSpeed : 40,
-    }
-    let canvas = document.getElementById("canvas");
-    let ctx = canvas.getContext("2d");
-    let w,h;
-    let lastPos;
-    let isDown = false;
-    let mouse = {x: 0, y:0}
-
     class Ball{
-        pos;
-        velocity;
+        #pos;
+        #velocity;
         isStoppedY =false;
         isStoppedX =false;
-        constructor() {
-            this.pos = {x: w/2,y:h/2};
+        #mouse;
+        #config = {
+            TWO_PI : Math.PI*2,
+            color: "rgba(255,236,140,0.85)", //"rgba(0,0,0,0.8)",
+            radius :100, //ball rad
+            air : 0.99, //air resistance for ball slowing
+            gravitation : 1, //"gravitation"
+            maxSpeed : 40,
+        }
+        /**
+         * @constructor
+         * @param context {CanvasRenderingContext2D} Context
+         * @param mouse {{x:Number,y:Number, keyPressed:Number}}
+         * @param width {Number}
+         * @param height {Number}
+         */
+        constructor(context, mouse,width,height) {
+            this.ctx = context;
+            this.#pos = {x: mouse.x/2,y:mouse.y/2};
             this.delta = {x:0,y: 1}
-            this.velocity ={x:1,y:0};
+            this.#velocity ={x:1,y:0};
+            this.#mouse = mouse;
+            this.w =width;
+            this.h = height;
         }
 
         draw(){
-            ctx.fillStyle = config.color;
-            ctx.beginPath();
-            ctx.arc(this.pos.x,this.pos.y,config.radius,0,config.TWO_PI);
-            ctx.fill();
+            this.ctx.fillStyle =this.#config.color;
+            this.ctx.beginPath();
+            this.ctx.arc(this.#pos.x,this.#pos.y,this.#config.radius,0,this.#config.TWO_PI);
+            this.ctx.fill();
         }
 
         velocityCalc(){
-            if(Math.abs(this.velocity.y)<config.acceleration*3&&
-                this.pos.y+config.radius>=h) {
+
+            //Stop checks for not clipping the floor
+            if(Math.abs(this.#velocity.y)<this.#config.gravitation*3&&
+                this.#pos.y+this.#config.radius>=h) {
                 this.delta.y = 0;
                 this.isStoppedY = true;
 
             }
-            if(Math.abs(this.velocity.x)<config.acceleration/3) {
+            if(Math.abs(this.#velocity.x)<this.#config.gravitation/3) {
                 this.delta.x =0;
                 this.isStoppedX = true;
             }
-            if(Math.abs(this.velocity.x)<config.acceleration)
+            if(Math.abs(this.#velocity.x)<this.#config.gravitation)
                 this.delta.x = 0;
-            if (this.velocity.y <= 0) {
+            if (this.#velocity.y <= 0) {
                 this.delta.y = 1;
             }
             //goes down when delta.y ==1 && velocity.y>0
-            this.velocity.x *=config.airX;
-            this.velocity.y +=config.acceleration*this.delta.y;
+            this.#velocity.x *=this.#config.air;
+            this.#velocity.y +=this.#config.gravitation*this.delta.y;
+            this.#velocity.y*= this.#config.air;
 
-            this.velocity.x = Math.min(config.maxSpeed, this.velocity.x);
-            this.velocity.y = Math.min(config.maxSpeed, this.velocity.y);
+            this.#velocity.x = Math.min(this.#config.maxSpeed, this.#velocity.x);
+            this.#velocity.y = Math.min(this.#config.maxSpeed, this.#velocity.y);
         }
 
         move(){
             if(!this.isStoppedY)
-                this.pos.y += this.delta.y * this.velocity.y;
+                this.#pos.y += this.delta.y * this.#velocity.y;
             if(!this.isStoppedX)
-                this.pos.x += this.delta.x * this.velocity.x;
+                this.#pos.x += this.delta.x * this.#velocity.x;
         }
         collisionCheck(){
-            if (this.pos.x + config.radius >= w)
+            if (this.#pos.x + this.#config.radius >= this.w) //floor collision
                 this.delta.x = -1;
-            if (this.pos.x - config.radius <= 0)
+            if (this.#pos.x - this.#config.radius <= 0) //roof collision
                 this.delta.x = 1;
-            if (this.pos.y - config.radius <= 0)
+            if (this.#pos.y - this.#config.radius <= 0) //left border collision
                 this.delta.y = 1;
-            if (this.pos.y + config.radius >= h) {
+            if (this.#pos.y + this.#config.radius >= this.h) { //duct tape method for ball slowing
                 this.delta.y = -1;
-                this.velocity.y*=config.air;
+                //this.velocity.y*=config.airY;
             }
         }
 
-        //delta sets to zero
+        //stops physics calculations for ball
         stop(){
             this.delta.x = this.delta.y = 0;
         }
+
+        //Resumes physics calculations for ball
         resume(){
-            ball.delta.x = (mouse.x-ball.pos.x)>0?1:-1;
-            ball.delta.y = (mouse.y -ball.pos.y)>0?1:-1;
-            debugger;
-            ball.velocity.x = Math.min(Math.abs((mouse.x - ball.pos.x))/20,
-                                config.maxSpeed);
-            ball.velocity.y = Math.min(Math.abs((mouse.y - ball.pos.y))/20,
-                                config.maxSpeed);
+
+            ball.delta.x = (this.#mouse.x-ball.#pos.x)>0?1:-1;
+            ball.delta.y = (this.#mouse.y -ball.#pos.y)>0?1:-1;
+            ball.#velocity.x = Math.min(Math.abs((this.#mouse.x - ball.#pos.x))/20,
+                this.#config.maxSpeed);
+            ball.#velocity.y = Math.min(Math.abs((this.#mouse.y - ball.#pos.y))/20,
+                this.#config.maxSpeed);
         }
 
+        //Draws line between ball and mouse pointer
         connectWithBall(){
-            ctx.beginPath();
-            ctx.moveTo(mouse.x, mouse.y);
-            ctx.lineTo(ball.pos.x,ball.pos.y);
-            ctx.stroke();
+            if(this.#mouse.keyPressed===0) {
+                this.ctx.beginPath();
+                this.ctx.moveTo(this.#mouse.x, this.#mouse.y);
+                this.ctx.lineTo(ball.#pos.x, ball.#pos.y);
+                this.ctx.stroke();
+            }
+        }
+
+        /**
+         *
+         * @param e {MouseEvent}
+         */
+        mouseDownEvent(e){
+            this.#mouse.keyPressed = e.button;
+            if(this.#mouse.keyPressed === 0) {
+                ball.stop();
+            }
+        }
+
+        /**
+         *
+         * @param e {MouseEvent}
+         */
+        mouseUpEvent(e){
+            this.#mouse.keyPressed = -1;
+            ball.isStoppedY = ball.isStoppedX = false;
+            ball.resume();
+        }
+
+        /**
+         *
+         * @param e {MouseEvent}
+         */
+        mouseMoveEvent(e){
+            this.#mouse.x = e.x;
+            this.#mouse.y = e.y;
         }
 
     }
+    let canvas = document.getElementById("canvas");
+    let ctx = canvas.getContext("2d");
+    let w = canvas.width = innerWidth;
+    let h = canvas.height = innerHeight;
+    let ball = new Ball(ctx,{x:400, y:400, keyPressed:-1},w,h);
+
+    loop();
     function loop(){
-        ctx.clearRect(0,0,w,h);
-        ctx.fillStyle = "#c55fff";
-        ctx.fillRect(0,0,w,h);
-        ctx.fillStyle =config.color;
+        reDrawBackground();
+        ball.draw();
         if (!(ball.isStoppedY&&ball.isStoppedX)) {
             ball.collisionCheck();
             ball.velocityCalc();
             ball.move();
-            if(isDown)
-                ball.connectWithBall();
+            ball.connectWithBall();
         }
-        ball.draw();
         window.requestAnimationFrame(loop);
     }
 
-    function init(){
-        w = canvas.width = innerWidth;
-        h = canvas.height = innerHeight;
-        isDown = false;
-
+    function reDrawBackground(){
+        ctx.clearRect(0,0,w,h);
+        ctx.fillStyle = "#c55fff";
+        ctx.fillRect(0,0,w,h);
     }
-    init();
 
-    let ball = new Ball(400,400);
-    ball.draw();
 
-    loop();
-
-    canvas.addEventListener("mousedown",(e)=>{
-        if(e.button == 0) {
-            isDown = true;
-            lastPos = {x: e.x, y: e.y};
-            ball.stop();
-            lastPos = {x: e.x, y: e.y};
-        }
+    //Listeners for mouse
+    this.canvas.addEventListener("mousedown",(e)=>{
+        ball.mouseDownEvent(e);
     })
-    canvas.addEventListener("mouseup",(e)=>{
-        if(e.button == 0) {
-            ball.isStoppedY = ball.isStoppedX = false;
-            isDown = false;
-            ball.resume();
-        }
+    this.canvas.addEventListener("mouseup",(e)=>{
+        ball.mouseUpEvent(e);
     })
 
-    canvas.addEventListener("mousemove",(e)=>{
-            mouse = {x: e.x, y: e.y};
+    this.canvas.addEventListener("mousemove",(e)=>{
+        ball.mouseMoveEvent(e);
     })
+
 })();
